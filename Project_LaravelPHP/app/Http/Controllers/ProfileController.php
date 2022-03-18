a871c3cc2477e9e26dac25d5d78128de112bf310
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-03-18 21:50:32 +0700

    load img

--- Project_LaravelPHP/app/Http/Controllers/ProfileController.php
+++ Project_LaravelPHP/app/Http/Controllers/ProfileController.php
@@ -9,4 +9,20 @@ class ProfileController extends Controller
     public function profile(){
         return view ('auth.profile_page');
     }
+
+    public function create(){
+
+    }
+
+    public function read(){
+        
+    }
+
+    public function update(){
+        
+    }
+
+    public function delete(){
+        
+    }
 }
